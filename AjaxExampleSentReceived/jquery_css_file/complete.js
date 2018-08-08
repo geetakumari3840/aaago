c8429637fc06e3c55dcc468b5541973cea92c3a4
geetakumari3840 <geetakumari3840@gmail.com>
2018-08-08 21:19:45 +0530

    asd

--- AjaxExampleSentReceived/jquery_css_file/complete.js
+++ AjaxExampleSentReceived/jquery_css_file/complete.js
@@ -1,54 +1,149 @@
 $(document).ready(function() {
-  // Use attr() to add an id, rel, and title.
-  $('div.chapter a[href*="wikipedia"]').attr({
-    rel: 'external',
-    title: function() {
-      return 'Learn more about ' + $(this).text() + ' at Wikipedia.';
-    },
-    id: function(index, oldValue) {
-      return 'wikilink-' + index;
-    }
+  $('#letter-a a').click(function(event) {
+    event.preventDefault();
+
+    $.ajaxSetup({
+      url: 'a.html',
+      type: 'POST',
+      dataType: 'html'
+    });
+
+    $.ajax({
+      type: 'GET',
+      success: function(data) {
+        $('#dictionary').html(data);
+      }
+    });
   });
 
-  // Add "back to top" links.
-  $('<a href="#top">back to top</a>').insertAfter('div.chapter p');
-  $('<a id="top"></a>').prependTo('body');
-
-  // Create footnotes.
-  var $notes = $('<ol id="notes"></ol>').insertBefore('#footer');
-  $('span.footnote').each(function(index) {
-    $(this)
-      .before([
-        '<a href="#footnote-',
-        index + 1,
-        '" id="context-',
-        index + 1,
-        '" class="context">',
-        '<sup>',
-        index + 1,
-        '</sup></a>'
-      ].join(''))
-      .appendTo($notes)
-      .append([
-        '&nbsp;(<a href="#context-',
-        index + 1,
-        '">context</a>)'
-      ].join(''))
-      .wrap('<li id="footnote-' + (index + 1) + '"></li>');
+  $('#letter-b a').click(function(event) {
+    event.preventDefault();
+    $.getJSON('b.json', function(data) {
+      var html = '';
+      $.each(data, function(entryIndex, entry) {
+        html += '<div class="entry">';
+        html += '<h3 class="term">' + entry.term + '</h3>';
+        html += '<div class="part">' + entry.part + '</div>';
+        html += '<div class="definition">';
+        html += entry.definition;
+        if (entry.quote) {
+          html += '<div class="quote">';
+          $.each(entry.quote, function(lineIndex, line) {
+            html += '<div class="quote-line">' + line + '</div>';
+          });
+          if (entry.author) {
+            html += '<div class="quote-author">' + entry.author + '</div>';
+          }
+          html += '</div>';
+        }
+        html += '</div>';
+        html += '</div>';
+      });
+      $('#dictionary').html(html);
+    });
   });
 
-  // Style pull quotes.
-  $('span.pull-quote').each(function(index) {
-    var $parentParagraph = $(this).parent('p');
-    $parentParagraph.css('position', 'relative');
+  $('#letter-c a').click(function(event) {
+    event.preventDefault();
+    $.getScript('c.js');
+  });
 
-    var $clonedCopy = $(this).clone();
-    $clonedCopy
-      .addClass('pulled')
-      .find('span.drop')
-        .html('&hellip;')
-      .end()
-      .text($clonedCopy.text())
-      .prependTo($parentParagraph);
+  $('#letter-d a').click(function(event) {
+    event.preventDefault();
+    $.get('d.xml', function(data) {
+      $('#dictionary').empty();
+      $(data).find('entry').each(function() {
+        var $entry = $(this);
+        var html = '<div class="entry">';
+        html += '<h3 class="term">' + $entry.attr('term');
+          html += '</h3>';
+        html += '<div class="part">' + $entry.attr('part');
+          html += '</div>';
+        html += '<div class="definition">';
+        html += $entry.find('definition').text();
+        var $quote = $entry.find('quote');
+        if ($quote.length) {
+          html += '<div class="quote">';
+          $quote.find('line').each(function() {
+            html += '<div class="quote-line">';
+              html += $(this).text() + '</div>';
+          });
+          if ($quote.attr('author')) {
+            html += '<div class="quote-author">';
+              html += $quote.attr('author') + '</div>';
+          }
+          html += '</div>';
+        }
+        html += '</div>';
+        html += '</div>';
+        $('#dictionary').append($(html));
+      });
+    });
+  });
+
+  $('#letter-e a').click(function(event) {
+    event.preventDefault();
+    var requestData = {term: $(this).text()};
+    $.get('e.php', requestData, function(data) {
+      $('#dictionary').html(data);
+    }).fail(function(jqXHR) {
+      $('#dictionary')
+      .html('Sorry, but an error occurred: ' + jqXHR.status)
+      .append(jqXHR.responseText);
+    });
+  });
+
+  $('#letter-f form').submit(function(event) {
+    event.preventDefault();
+    var formValues = $(this).serialize();
+    $.get('f.php', formValues, function(data) {
+      $('#dictionary').html(data);
+    });
+  });
+
+  var url = 'http://examples.learningjquery.com/jsonp/g.php';
+  $('#letter-g a').click(function(event) {
+    event.preventDefault();
+    $.getJSON(url + '?callback=?', function(data) {
+      var html = '';
+      $.each(data, function(entryIndex, entry) {
+        html += '<div class="entry">';
+        html += '<h3 class="term">' + entry.term + '</h3>';
+        html += '<div class="part">' + entry.part + '</div>';
+        html += '<div class="definition">';
+        html += entry.definition;
+        if (entry.quote) {
+          html += '<div class="quote">';
+          $.each(entry.quote, function(lineIndex, line) {
+            html += '<div class="quote-line">' + line + '</div>';
+          });
+          if (entry.author) {
+            html += '<div class="quote-author">' + entry.author + '</div>';
+          }
+          html += '</div>';
+        }
+        html += '</div>';
+        html += '</div>';
+      });
+      $('#dictionary').html(html);
+    });
+  });
+
+  $('#letter-h a').click(function(event) {
+    event.preventDefault();
+    $('#dictionary').load('h.html .entry');
+  });
+
+  var $loading = $('<div id="loading">Loading...</div>')
+    .insertBefore('#dictionary');
+
+  $(document).ajaxStart(function() {
+    $loading.show();
+  }).ajaxStop(function() {
+    $loading.hide();
+  });
+
+  $('body').on('click', 'h3.term', function() {
+    $(this).siblings('.definition').slideToggle();
   });
 });
--- AjaxExampleSentReceived/jquery_css_file/myjs.js
+++ AjaxExampleSentReceived/jquery_css_file/myjs.js
@@ -1,7 +1,49 @@
 $(document).ready(function(){
+	
+//Add -A- add a.html 
 	$('#letter-a a').click(function(event){
 		event.preventDefault();
-		$('#dictionary').load('a.html');
+		$('#dictionary').load('OthersFiles/a.html');
 		alert('Loaded!');
 	});
+
+
+//Add -b.json file on B Menu  page 149
+
+	$('#letter-b a').click(function(event){
+		event.preventDefault();
+		$.getJSON('OthersFiles/b.json', function(data){
+			var html='';
+			$.each(data,function(entryIndex,entry) {
+				html += '<div class="entry">';
+				html += '<h3 class="term">' + entry.term + '</h3>';
+				html += '<div class="part">' + entry.part + '</div>';
+				html += '<div class="definition">';
+				html += entry.definition;
+				if (entry.quote) {
+				html += '<div class="quote">';
+				$.each(entry.quote, function(lineIndex, line) {
+				html += '<div class="quote-line">' + line + '</div>';
+				});
+				if (entry.author) {
+				html += '<div class="quote-author">' + entry.author + '</div>';
+				}
+				html += '</div>';
+}
+				html += '</div>';
+				html += '</div>';
+			});
+			$('#dictionary').html(html);
+		});
+	});
+
+//Add -C- add a.html 
+	$('#letter-c a').click(function(event){
+		event.preventDefault();
+		$.getScript('OthersFiles/c.js')
+	});
+
+
+
+
 });
