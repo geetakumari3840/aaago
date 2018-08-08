359b378a8ea479b36bff2db948dbc3da42c365ee
“Geeta <geetakumari3840@gmail.com>
2018-08-08 15:58:25 +0530

    change

--- Manipulatingthe_DOM/jquery_css_file/myjs.js
+++ Manipulatingthe_DOM/jquery_css_file/myjs.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
   // Use attr() to add an id, rel, and title.
 //$("#h-title").addClass("w3-green");
-$('#container').append('<p class="w3-jumbo w3-purple">Hello</p>');
+$('#container').append('<p class="w3-jumbo w3-amber">Hello</p>');
 
 $('#h-title').on('click', function(){
 $('.title').addClass('w3-green');
+$('.atitle').addClass('w3-purple');
 });
 
 });
