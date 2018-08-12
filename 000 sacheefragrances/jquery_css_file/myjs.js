02ffc3d91ad8e80db13bf2733ecf2306bbb593ef
geetakumari3840 <geetakumari3840@gmail.com>
2018-08-12 21:32:26 +0530

    Ad

---
+++ 000 sacheefragrances/jquery_css_file/myjs.js
@@ -0,0 +1,11 @@
+// localhost/aaago/sacheefragrances/index.html
+
+$(document).ready(function(){ 
+
+	$('body').addClass('boody')
+	$('#first').addClass('first');
+	$('#second').addClass('second');
+
+//	$('body').css({background-color:'grey', margin: '0px'});
+//READ DOCUMENT CLOSE 183
+});
---
+++ test/jquery_css_file/myjs.js
@@ -0,0 +1,11 @@
+http://localhost/aaago/07.%20Using%20Plugins/index.html
+
+$(document).ready(function(){ 
+
+	$('#books').cyclecycle({ 
+		timeout: 2000, 
+		speed: 200, 
+		pause: true });
+
+//READ DOCUMENT CLOSE 183
+});
