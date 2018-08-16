e7813a07a98807714f6c381cd062ca71a4ff2315
“Geeta <geetakumari3840@gmail.com>
2018-08-16 17:48:09 +0530

    afa

--- 07. Using Plugins/jquery_css_file/myjs.js
+++ 07. Using Plugins/jquery_css_file/myjs.js
@@ -2,10 +2,23 @@ http://localhost/aaago/07.%20Using%20Plugins/index.html
 
 $(document).ready(function(){ 
 
-	$('#books').cyclecycle({ 
-		timeout: 2000, 
-		speed: 200, 
-		pause: true });
+$('#books').cycle({ timeout: 2000, speed: 200, pause: true });
+
+var $books = $('#books');
+var $controls = $('<div id="books-controls"></div>');
+$controls.insertAfter($books);
+
+$('<button>Pause</button>').click(function(event) { 
+	event.preventDefault(); 
+	$books.cycle('pause');
+	$.cookie('cyclePaused', 'y');
+	}).appendTo($controls);
+	$('<button>Resume</button>').click(function(event) {
+	event.preventDefault();
+	$('ul:paused').cycle('resume');
+	$.cookie('cyclePaused', null);
+	}).appendTo($controls);
 
 //READ DOCUMENT CLOSE 183
+
 });
